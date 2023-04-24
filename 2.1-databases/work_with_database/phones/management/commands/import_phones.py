f48440a839ae9a5f1be8e2c20e55f1c1da5eb909
Eugene Smyshlyaev <e.smyshl@gmail.com>
2023-04-24 19:58:21 +0400

    Django homework 2.1

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -1,6 +1,7 @@
 import csv
 
 from django.core.management.base import BaseCommand
+from django.utils.text import slugify
 from phones.models import Phone
 
 
@@ -14,4 +15,12 @@ class Command(BaseCommand):
 
         for phone in phones:
             # TODO: Добавьте сохранение модели
-            pass
+            item = Phone(id=int(phone['id']), name=phone['name'], image=phone['image'], price=float(phone['price']),
+                         release_date=phone['release_date'])
+            if phone['lte_exists'] == 'True':
+                item.lte_exists = 1
+            else:
+                item.lte_exists = 0
+            item.slug = slugify(item.name)
+            item.save()
+            # pass
--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 
+from phones.models import Phone
+
 
 def index(request):
     return redirect('catalog')
@@ -7,11 +9,36 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    sort_field = 'id'
+
+    sort_param = request.GET.get('sort')
+    if sort_param == 'name':
+        sort_field = 'name'
+    elif sort_param == 'min_price':
+        sort_field = 'price'
+    elif sort_param == 'max_price':
+        sort_field = '-price'
+
+    phone_objects = Phone.objects.all().order_by(sort_field)
+    context = {
+        'phones': [
+            {
+                'name': i.name, 'price': i.price, 'image': i.image, 'release_date': i.release_date,
+                'lte_exists': i.lte_exists, 'slug': i.slug} for i in phone_objects
+        ]
+    }
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+
+    phone_objects = Phone.objects.filter(slug=slug)[0]
+
+    context = {
+        'phone': {
+                'name': phone_objects.name, 'price': phone_objects.price, 'image': phone_objects.image,
+                'release_date': phone_objects.release_date, 'lte_exists': phone_objects.lte_exists, 'slug': phone_objects.slug
+        }
+    }
     return render(request, template, context)
