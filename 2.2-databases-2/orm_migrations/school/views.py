c62b2099b6cb01bfe1faef0eec7788a37fb3026e
Eugene Smyshlyaev <e.smyshl@gmail.com>
2023-07-08 23:26:37 +0300

    Homework_2.2_part_1_2

--- 2.2-databases-2/orm_migrations/school/views.py
+++ 2.2-databases-2/orm_migrations/school/views.py
@@ -10,13 +10,13 @@ def students_list(request):
     students_object_list = Student.objects.all().prefetch_related('teachers')
     teacher_object_list = Teacher.objects.all()
 
-    print(students_object_list)
-    print(teacher_object_list)
-
-    for student in students_object_list:
-        print(student.name, '-', student.teachers.all())
-        if not student.teachers.all():
-            student.teachers.add(teacher_object_list.get(id=student.teacher))
+    # print(students_object_list)
+    # print(teacher_object_list)
+    #
+    # for student in students_object_list:
+    #     print(student.name, '-', student.teachers.all())
+    #     if not student.teachers.all():
+    #         student.teachers.add(teacher_object_list.get(id=student.teacher))
 
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
